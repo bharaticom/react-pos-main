7047fe763b1b88a10599083c6e0cfef1d237404a
panchakarlabk <bharat_icom@icomsol.co.uk>
2023-03-30 12:12:38 +0100

    add initi

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+
+function HomePage() {
+  return (
+    <MainLayout>
+        <div className='bg-light p-5 mt-4 rounded-3'>
+            <h1>Welcome to Hackfest for Retail Use Case</h1>
+            <Link to='/pos' className='btn btn-primary'>Click here to sell products</Link>
+        </div>
+    </MainLayout>
+  )
+}
+
+export default HomePage
